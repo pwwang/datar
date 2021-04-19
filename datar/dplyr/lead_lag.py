b79f2409f068bfee0250ebcb8588c514c2090320
pwwang <pwwang@pwwang.com>
2021-04-19 10:36:23 -0700

    Add tests for lead-lag

--- datar/dplyr/lead_lag.py
+++ datar/dplyr/lead_lag.py
@@ -2,13 +2,13 @@
 
 https://github.com/tidyverse/dplyr/blob/master/R/lead-lag.R
 """
-# TODO: add tests
 from typing import Iterable, Any, Optional
 
-from pandas import Series
+from pandas import Series, Categorical
+from pandas.core.dtypes.common import is_categorical_dtype
 from pipda import register_func
 
-from ..core.types import NumericType
+from ..core.types import NumericType, is_scalar
 from ..core.contexts import Context
 from ..base.constants import NA
 
@@ -31,21 +31,16 @@ def lead(
     Returns:
         Lead or lag values with default values filled to series.
     """
-    if not isinstance(series, Series):
-        series = Series(series)
-
+    series, cats, default = lead_lag_prepare(series, n, default, order_by)
     index = series.index
-    if order_by is not None:
-        if not isinstance(order_by, Series):
-            order_by = Series(order_by)
-        order_by = order_by.sort_values()
-        series = series[order_by.index]
 
-    ret = [default] * len(series)
+    ret = default * len(series)
     ret[:-n] = series.values[n:]
     if order_by is not None:
         ret = Series(ret, index=order_by.index)
-        return ret[index]
+        ret = ret[index]
+    if cats is not None:
+        ret = Categorical(ret, categories=cats)
     return Series(ret, index=index)
 
 @register_func(None, context=Context.EVAL)
@@ -59,19 +54,46 @@ def lag(
 
     See lead()
     """
-    if not isinstance(series, Series):
-        series = Series(series)
-
+    series, cats, default = lead_lag_prepare(series, n, default, order_by)
     index = series.index
+
+    ret = default * len(series)
+    ret[n:] = series.values[:-n]
+    if order_by is not None:
+        ret = Series(ret, index=order_by.index)
+        ret = ret[index]
+    if cats is not None:
+        ret = Categorical(ret, categories=cats)
+    return Series(ret, index=index)
+
+def lead_lag_prepare(
+        data: Iterable[Any],
+        n: int,
+        default: Any,
+        order_by: Optional[Iterable[Any]]
+):
+    """Prepare and check arguments for lead-lag"""
+    cats = None
+    if is_categorical_dtype(data):
+        if isinstance(data, Series):
+            data = data.cat
+        cats = data.categories
+
+    if not isinstance(n, int) or n < 0:
+        raise ValueError(f"`lead-lag` expect a non-negative integer for `n`.")
+
+    if is_scalar(default):
+        default = [default]
+    if len(default) != 1:
+        raise ValueError(f"`lead-lag` Expect scalar or length-1 `default`.")
+
+    if not isinstance(data, Series):
+        data = Series(data)
+
     if order_by is not None:
         if not isinstance(order_by, Series):
             order_by = Series(order_by)
         order_by = order_by.sort_values()
-        series = series[order_by.index]
+        data = data[order_by.index]
 
-    ret = [default] * len(series)
-    ret[n:] = series.values[:-n]
-    if order_by is not None:
-        ret = Series(ret, index=order_by.index)
-        return ret[index]
-    return Series(ret, index=index)
+    return data, cats, default
