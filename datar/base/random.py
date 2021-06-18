0485a5ab4b315883a0674e8eeb5baf7f920daaaf
pwwang <pwwang@pwwang.com>
2021-06-17 17:41:43 -0700

    Add base.random module

---
+++ datar/base/random.py
@@ -0,0 +1,9 @@
+"""Functions related to randomization"""
+import random as std_random
+
+import numpy
+
+def set_seed(seed: int) -> None:
+    """Set the seed for randomization"""
+    std_random.seed(seed)
+    numpy.random.seed(seed)
